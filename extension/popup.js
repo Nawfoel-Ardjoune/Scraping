68330a3a8e9bd3a245e34df6275daf9c8beab274
PC-NAWFOEL <ardjoune.nawfoel@outlook.fr>
2024-08-13 12:17:58 +0200

    petit changement pour recherche

--- extension/popup.js
+++ extension/popup.js
@@ -1,5 +1,7 @@
 //Action du click sur le bouton de collecte
 document.getElementById('scrape').addEventListener('click', () => {
+  const nbPages = document.getElementById('nbPages').value;
+  console.log("Nombre de pages == ", nbPages);
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
